ccc75e2ec0568164a1948d79f0d48017dfa60647
Scott Jones <scottdj92@gmail.com>
2016-12-19 14:54:10 -0500

    Add extra packages and webpack config

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+
+const PATHS = {
+    app: path.join(__dirname, 'app'),
+    build: path.join(__dirname, 'build')
+};
+
+const config = {
+    entry: {
+        app: PATHS.app
+    },
+    output: {
+        path: PATHS.build,
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Webpack Boilerplate'
+        })
+    ],
+    resolve: {
+        modules: ['node_modules'],
+        extensions: ['.css', '.js', '.jsx', '.scss']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                include: PATHS.app,
+                use: ['react-hot-loader', 'babel-loader']
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.scss$/,
+                use: ['css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(jpg|svg|png|gif)$/,
+                use: ['image-webpack-loader'],
+                query: {
+                    bypassOnDebug: true,
+                    optimizationlevel: 7,
+                    interlaced: false
+                }
+            }
+        ]
+    }
+};
+
+module.exports = function(env) {
+    return merge(config);
+};
